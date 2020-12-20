d2e3fb88ee3b745c1a0749b826695813935c9f9f
Phil <phil.brockman@gmail.com>
2020-12-20 12:34:13 -0800

    changes to root directory

--- src/index.js
+++ src/index.js
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+const routerBaseName = process.env.PUBLIC_URL;
+
+console.log(routerBaseName)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router basename={routerBaseName}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Router>,
   document.getElementById('root')
 );
 
